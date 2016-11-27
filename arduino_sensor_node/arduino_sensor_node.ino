b8adf7dd51e7ec07217cd503e3cba438983469b9
LeoTheHuman <leothehuman@gmail.com>
2016-11-27 04:46:54 +0400

    Added lights control

--- arduino_sensor_node/arduino_sensor_node.ino
+++ arduino_sensor_node/arduino_sensor_node.ino
@@ -48,14 +48,14 @@ public:
                 default:
                     Serial.println("Device is not a DS18x20 family device.");
                     badCRC = true;
-                } 
+                }
                 for (byte i = 0; i < 7; i++) // we need 9 bytes
                 {
                     Serial.print(addr_[i], HEX);
                     Serial.print(" ");
                 }
                 Serial.println();
-            }        
+            }
         }
         while (badCRC);
         ds_.reset();
@@ -74,17 +74,17 @@ public:
             return;
         }
         nextCheck_ = now + 10000;
-      
+
         byte present = ds_.reset();
-        ds_.select(addr_);    
+        ds_.select(addr_);
         ds_.write(0xBE); // Read Scratchpad
-    
+
         byte data[12];
         for (byte i = 0; i < 9; i++) // we need 9 bytes
         {
             data[i] = ds_.read();
         }
-    
+
         //Serial.print("  Data = ");
         //Serial.print(present, HEX);
         //Serial.print(" ");
@@ -96,20 +96,20 @@ public:
         //Serial.print(" CRC=");
         //Serial.print(OneWire::crc8(data, 8), HEX);
         //Serial.println();
-    
+
         byte crc = OneWire::crc8(data, 8);
         if (crc != data[8])
         {
             Serial.print("CRC error");
             return;
         }
-        
+
         // Convert the data to actual temperature
         // because the result is a 16 bit signed integer, it should
         // be stored to an "int16_t" type, which is always 16 bits
         // even when compiled on a 32 bit processor.
         int16_t raw = (data[1] << 8) | data[0];
-        if (type_) 
+        if (type_)
         {
             raw = raw << 3; // 9 bit resolution default
             if (data[7] == 0x10) {
@@ -129,7 +129,7 @@ public:
                 raw = raw & ~3;
                 break;
             case 0x40: // 10 bit res, 187.5 ms
-                raw = raw & ~1; 
+                raw = raw & ~1;
                 break;
             default: //// default is 12 bit resolution, 750 ms conversion time
                 break;
@@ -156,7 +156,7 @@ int idDHT11pin = 2; //Digital pin for comunications
 int idDHT11intNumber = 0; //interrupt number (must be the one that use the previus defined pin (see table above)
 void dht11_wrapper(); // must be declared before the lib initialization
 idDHT11 DHT11(idDHT11pin,idDHT11intNumber,dht11_wrapper);
-void dht11_wrapper() 
+void dht11_wrapper()
 {
     DHT11.isrCallback();
 }
@@ -180,25 +180,33 @@ DS dsSensor(oneWire);
 int lightPin = A0;  //define a pin for Photo resistor
 int light = 0;
 
+unsigned char lightsBrightness = 0;
+
 int soundPin = 7;
 unsigned long sound = 0;
 
 const unsigned long SoundDelay = 1000;
 const unsigned long PirDelay = 1000;
 
+int ledControlPin[3] = {5, 3, 6}; // Must be a PWM pin -- 3, 5, 6, 9, 10, 11
+
 void setup()
 {
     delay(1000);
     Serial.begin(9600);
-    
+
     dsSensor.init();
-     
+
     pinMode(pirPin, INPUT);
     pinMode(lightPin, INPUT);
     pinMode(soundPin, INPUT);
     pinMode(ledPin, OUTPUT);
+    for (int j = 0; j < 3; ++j)
+    {
+        pinMode(ledControlPin[j], OUTPUT);
+    }
 
-    IPAddress myIP(192,168,0,2);
+    IPAddress myIP(192,168,1,5);
 
     Ethernet.begin(dsSensor.getAddress() + 1, myIP);
 
@@ -226,8 +234,8 @@ void handleDHTstate(unsigned long now)
         DHT11.acquire();
         nextDHTcheck = now + 10000;
         state = DHT_WAIT;
-    } 
-    
+    }
+
     if (state == DHT_WAIT && !DHT11.acquiring())
     {
         int result = DHT11.getStatus();
@@ -279,33 +287,96 @@ void handleHttpServer(unsigned long now)
     {
         return;
     }
-//    size_t size = client.available();
-//    if(size > 0)
-//    {
-//        uint8_t* msg = (uint8_t*)malloc(size);
-//        size = client.read(msg, size);
-//        Serial.write(msg, size);
-//        free(msg);
-//    }
-    client.print(temperature, 2);
-    client.print("oC, ");
-    client.print(humidity, 2);
-    client.print("%, ");
-//    client.print((now - lastChecked)/1000, DEC);
-//    client.println(" sec ago");
-    client.print(dsSensor.getTemperature(), 2);
-    client.print("oC, ");
-    client.print(light, DEC);
-    client.print(" light level, ");
-    client.print(sound > now ? "sound, " : "no sound, ");
-    client.println(pir > now ? "someone in" : "no one in");
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected())
+    {
+        if (client.available())
+        {
+            char c = client.read();
+            Serial.write(c);
+            if (c == '\n' && currentLineIsBlank)
+            {
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-Type: application/json");
+                client.println("Connection: close");
+                client.println("Refresh: 5");
+                client.println();
+                client.print("{ temperature: \"");
+                client.print(temperature, 2);
+                client.print("\", humidity: \"");
+                client.print(humidity, 2);
+                client.print("\", temp: \"");
+                client.print(dsSensor.getTemperature(), 2);
+                client.print("\", light: \"");
+                client.print(light, DEC);
+                client.print("\", brightness: \"");
+                client.print(lightsBrightness, DEC);
+                client.print("\", sound: \"");
+                client.print(sound > now ? "true" : "false");
+                client.print("\", pir: \"");
+                client.print(pir > now ? "true" : "false");
+                client.println("\" }");
+                break;
+            }
+            if (c == '\n')
+            {
+                currentLineIsBlank = true;
+            }
+            else if (c != '\r')
+            {
+                currentLineIsBlank = false;
+            }
+        }
+    }
+    delay(100);
     client.stop();
+    Serial.println("client disconnected");
+}
+
+unsigned char rgb[3] = {0, 0, 0};
+void recolor(unsigned char brightness)
+{
+    rgb[0] = brightness;
+//    rgb[1] = brightness/4;
+    rgb[2] = brightness/2;
+    for (int j = 0; j < 3; ++j)
+    {
+        analogWrite(ledControlPin[j], rgb[j]);
+    }
+}
+
+unsigned long nextLightsUpdate = 0;
+void handleLights(unsigned long now)
+{
+    if (now < nextLightsUpdate)
+    {
+        return;
+    }
+    nextLightsUpdate = now + 25;
+    if (pir > now && light < 200)
+    {
+        if (lightsBrightness < 255)
+        {
+            lightsBrightness += 1;
+            recolor(lightsBrightness);
+        }
+    }
+    else
+    {
+        if (lightsBrightness > 0)
+        {
+            lightsBrightness -= 1;
+            recolor(lightsBrightness);
+        }
+    }
 }
 
 void loop()
 {
     unsigned long now = millis();
-   
+
     dsSensor.process(now);
     time("dsSensor.process", now);
     handleDHTstate(now);
@@ -320,6 +391,9 @@ void loop()
     time("handleLightSensor", now);
 
     digitalWrite(ledPin, sound > now | pir > now);
+
+    handleLights(now);
+    time("handleLights", now);
 
     handleHttpServer(now);
     time("handleHttpServer", now);
